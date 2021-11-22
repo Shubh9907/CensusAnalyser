1959b1495c66a6113cf66e2381880970405b27c2
Shubh9907 <Shubhamv60047@gmail.com>
2021-11-22 21:39:12 +0530

    Added:- Welcome Message Added

---
+++ src/main/java/com/bridgelabz/CensusAnalyserMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class CensusAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian State Census Analyser Program");
+    }
+}
